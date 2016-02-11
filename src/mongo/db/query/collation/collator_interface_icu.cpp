a68b83e539ae85b2bbd529342cc87f4787b8c831
David Storch <david.storch@10gen.com>
2016-02-11 16:28:31 -0500

    WIP CollatorInterface

---
+++ src/mongo/db/query/collation/collator_interface_icu.cpp
@@ -0,0 +1,54 @@
+/**
+ *    Copyright (C) 2016 MongoDB Inc.
+ *
+ *    This program is free software: you can redistribute it and/or  modify
+ *    it under the terms of the GNU Affero General Public License, version 3,
+ *    as published by the Free Software Foundation.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    As a special exception, the copyright holders give permission to link the
+ *    code of portions of this program with the OpenSSL library under certain
+ *    conditions as described in each individual source file and distribute
+ *    linked combinations including the program with the OpenSSL library. You
+ *    must comply with the GNU Affero General Public License in all respects for
+ *    all of the code used other than as permitted herein. If you modify file(s)
+ *    with this exception, you may extend this exception to your version of the
+ *    file(s), but you are not obligated to do so. If you do not wish to do so,
+ *    delete this exception statement from your version. If you delete this
+ *    exception statement from all source files in the program, then also delete
+ *    it in the license file.
+ */
+
+#include "mongo/platform/basic.h"
+
+#include "mongo/db/query/collation/collator_interface_icu.h"
+
+#include "mongo/util/assert_util.h"
+
+namespace mongo {
+
+CollatorInterfaceICU::CollatorInterfaceICU(std::unique_ptr<icu::Collator> collator)
+    : _collator(std::move(collator)) {
+    invariant(_collator);
+}
+
+int CollatorInterfaceICU::compare(const char* left,
+                                  uint32_t leftLen,
+                                  const char* right,
+                                  uint32_t rightLen) {
+    UErrorCode status = U_ZERO_ERROR;
+    auto compareResult = _collator->compareUTF8(
+        icu::StringPiece(left, leftLen), icu::StringPiece(right, rightLen), status);
+    invariant(U_SUCCESS(status));
+
+    return compareResult;
+}
+
+}  // namespace mongo
